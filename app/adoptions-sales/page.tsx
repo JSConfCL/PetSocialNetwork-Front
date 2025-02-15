9263f10fc64e53ec57e5e4e610698c01fbd5b03b
kevinkeyssx <keyss1700@gmail.com>
2025-02-15 20:24:54 -0300

    AdoptionSalesCard added

--- app/adoptions-sales/page.tsx
+++ app/adoptions-sales/page.tsx
@@ -1,12 +1,7 @@
 'use client';
 
-import Image from 'next/image';
-
-import { Heart, DollarSign } from 'lucide-react';
-
-import { pets }     from '../data/pets';
-import { Card }     from '@/components/ui/card';
-import { Badge }    from '@/components/ui/badge';
+import { pets }         from '../data/pets';
+import AdotionSalesCard from '../components/AdoptionsSales/AdotionSalesCard';
 
 
 export default function AdoptionsSalesPage() {
@@ -17,61 +12,10 @@ export default function AdoptionsSalesPage() {
 
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                     {pets.map((pet) => (
-                        <Card key={pet.id} className="overflow-hidden shadow-md hover:scale-[1.01] transition-transform duration-300">
-                            <div className="relative h-64">
-                                <Image
-                                    src={pet.image}
-                                    alt={pet.name}
-                                    fill
-                                    className="object-cover"
-                                />
-
-                                <Badge
-                                    className={`absolute top-4 right-4 ${
-                                        pet.status === 'adoption'
-                                        ? 'bg-green-500/90 hover:bg-green-500/90'
-                                        : 'bg-blue-500/90 hover:bg-blue-500/90'
-                                    }`}
-                                >
-                                    {pet.status === 'adoption' ? (
-                                        <Heart className="h-4 w-4 mr-1" />
-                                    ) : (
-                                        <DollarSign className="h-4 w-4 mr-1" />
-                                    )}
-                                    {pet.status === 'adoption' ? 'Me adoptan' : 'Me venden'}
-                                </Badge>
-                            </div>
-
-                            <div className="p-6">
-                                <div className="flex items-center justify-between mb-2">
-                                    <h2 className="text-xl font-semibold">{pet.name}</h2>
-                                    <span className="text-sm text-muted-foreground">{pet.age}</span>
-                                </div>
-
-                                <p className="text-muted-foreground mb-4">{pet.description}</p>
-
-                                <div className="space-y-3">
-                                    <div>
-                                        <span className="font-medium">Personalidad:</span>{' '}
-                                        <span className="text-muted-foreground">{pet.personality}</span>
-                                    </div>
-
-                                    {pet.status === 'sale' && pet.price && (
-                                        <div className="font-medium text-lg flex items-center">
-                                            Precio:
-                                            <span className='flex items-center ml-2'>
-                                                <DollarSign className="h-4 w-4 " />
-                                                {pet.price}
-                                            </span>
-                                        </div>
-                                    )}
-
-                                    <div className="bg-secondary/50 rounded-lg p-4 italic text-sm">
-                                        "{pet.message}"
-                                    </div>
-                                </div>
-                            </div>
-                        </Card>
+                        <AdotionSalesCard
+                            key={pet.id}
+                            {...pet}
+                        />
                     ))}
                 </div>
             </div>
---
+++ app/components/AdoptionsSales/AdotionSalesCard.tsx
@@ -0,0 +1,72 @@
+import { JSX } from "react";
+
+import Image from 'next/image';
+
+import { Heart, DollarSign } from 'lucide-react';
+
+import { Card }     from "@/components/ui/card";
+import { Badge }    from "@/components/ui/badge";
+import { Pet }      from "../../models/pet.model";
+
+
+export default function AdotionSalesCard(
+    pet: Pet
+): JSX.Element {
+    return (
+        <Card key={pet.id} className="overflow-hidden shadow-md hover:scale-[1.01] transition-transform duration-300">
+        <div className="relative h-64">
+            <Image
+                src={pet.image}
+                alt={pet.name}
+                fill
+                className="object-cover"
+            />
+
+            <Badge
+                className={`absolute top-4 right-4 ${
+                    pet.status === 'adoption'
+                    ? 'bg-green-500/90 hover:bg-green-500/90'
+                    : 'bg-blue-500/90 hover:bg-blue-500/90'
+                }`}
+            >
+                {pet.status === 'adoption' ? (
+                    <Heart className="h-4 w-4 mr-1" />
+                ) : (
+                    <DollarSign className="h-4 w-4 mr-1" />
+                )}
+                {pet.status === 'adoption' ? 'Me adoptan' : 'Me venden'}
+            </Badge>
+        </div>
+
+        <div className="p-6">
+            <div className="flex items-center justify-between mb-2">
+                <h2 className="text-xl font-semibold">{pet.name}</h2>
+                <span className="text-sm text-muted-foreground">{pet.age}</span>
+            </div>
+
+            <p className="text-muted-foreground mb-4">{pet.description}</p>
+
+            <div className="space-y-3">
+                <div>
+                    <span className="font-medium">Personalidad:</span>{' '}
+                    <span className="text-muted-foreground">{pet.personality}</span>
+                </div>
+
+                {pet.status === 'sale' && pet.price && (
+                    <div className="font-medium text-lg flex items-center">
+                        Precio:
+                        <span className='flex items-center ml-2'>
+                            <DollarSign className="h-4 w-4 " />
+                            {pet.price}
+                        </span>
+                    </div>
+                )}
+
+                <div className="bg-secondary/50 rounded-lg p-4 italic text-sm">
+                    "{pet.message}"
+                </div>
+            </div>
+        </div>
+    </Card>
+    );
+}
