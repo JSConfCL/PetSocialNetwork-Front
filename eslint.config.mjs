181e966f69afc3b979a35a38bdbf1d6421e6c71b
mario <sks_894@hotmail.com>
2025-02-19 01:51:52 -0300

    Add import-helpers to eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import importHelpers from "eslint-plugin-import-helpers";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -11,6 +11,36 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      "import-helpers": importHelpers,
+    },
+    rules: {
+      "import-helpers/order-imports": [
+        "error",
+        {
+          newlinesBetween: "always",
+          groups: [
+            ["/^next/", "module"],
+            "/^@/styles/",
+            "/^@/app/",
+            "/^@/models/",
+            "/^@/data-access/",
+            "/^@/lib/",
+            "/^@/providers/",
+            "/^@/hooks/",
+            "/^@/components/",
+            ["parent", "sibling", "index"],
+          ],
+          alphabetize: {
+            order: "asc",
+            ignoreCase: true,
+          },
+        },
+      ],
+      "@typescript-eslint/no-empty-object-type": "off",
+      "no-console": "warn",
+    },
+  },
 ];
-
 export default eslintConfig;
