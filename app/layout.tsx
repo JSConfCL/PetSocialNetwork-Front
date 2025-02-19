0d0cc4dc829526779a7ed51ddaefa9fe66b6b99a
mario <sks_894@hotmail.com>
2025-02-19 01:50:02 -0300

    Update next-themes config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,48 +1,38 @@
-import type { Metadata } from "next";
-
 import "./globals.css";
 
-import { Header }           from "./components/Header";
-import { Menu }             from "./components/Menu/Menu";
-import { PawBackground }    from "./components/PawBackground";
-import { ThemeProvider }    from "./components/theme-provider";
+import type { Metadata } from "next";
+import { ThemeProvider } from "next-themes";
+import { PropsWithChildren } from "react";
 
+import { Header } from "@/components/Header";
+import { Menu } from "@/components/Menu/Menu";
+import { PawBackground } from "@/components/PawBackground";
 
 export const metadata: Metadata = {
-    title       : "PetSocialNetwork",
-    description : "Una red social para mascotas",
+  title: "PetSocialNetwork",
+  description: "Una red social para mascotas",
 };
 
-
-export default function RootLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-    return (
-        <html lang="es">
-            <body>
-                <ThemeProvider
-                    attribute       = "class"
-                    defaultTheme    = "system"
-                    enableSystem
-                    disableTransitionOnChange
-                >
-                    <PawBackground />
-
-                    <Header />
-
-                    <div className="container mx-auto p-4">
-                        <div className="flex flex-col lg:flex-row gap-6">
-                            <Menu />
-
-                            <div className="flex-1 z-20">
-                                { children }
-                            </div>
-                        </div>
-                    </div>
-                </ThemeProvider>
-            </body>
-        </html>
-    );
+export default function RootLayout({ children }: PropsWithChildren) {
+  return (
+    <html lang="es" suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <PawBackground />
+          <Header />
+          <div className="container mx-auto p-4">
+            <div className="flex flex-col gap-6 lg:flex-row">
+              <Menu />
+              <div className="z-20 flex-1">{children}</div>
+            </div>
+          </div>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
