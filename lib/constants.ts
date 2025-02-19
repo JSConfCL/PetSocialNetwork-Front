0c13c524602ca59cb30e5ea90e85a4fa843d2881
mario <sks_894@hotmail.com>
2025-02-19 01:54:19 -0300

    Add constants for test IDs and application links

---
+++ lib/constants.ts
@@ -0,0 +1,17 @@
+export const TEST_IDS = Object.freeze({
+  profileButton: "profileButton",
+  userMenuButtonTrigger: "userMenuButtonTrigger",
+  modalCloseButton: "modalCloseButton",
+  modalInfoMessage: "modalInfoMessage",
+  logoutButtonTrigger: "logoutButtonTrigger",
+  logoutButton: "logoutButton",
+});
+
+export const APP_LINKS = Object.freeze({
+  HOME_PAGE: "/",
+  LOGIN_PAGE: "/iniciar-sesion",
+  CREATE_ACCOUNT_PAGE: "/crear-cuenta",
+  USER_PAGE: "/usuario",
+  FORGOT_PASSWORD_PAGE: "/olvide-clave",
+  RESET_PASSWORD_PAGE: "/restablecer-clave",
+});
