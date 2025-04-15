8e47ed94bfa8536094742b4533956fd9808210eb
kevinkeyssx <keyss1700@gmail.com>
2025-04-15 16:20:33 -0400

    images.domaind is deprecated, now remotePatterns use

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: [
-      "images.unsplash.com",
-      "s3.amazonaws.com",
-      "www.purina.in",
-      "pridebites.com",
-      "www.muyinteresante.com",
-    ]
-  }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "s3.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.purina.in",
+      },
+      {
+        protocol: "https",
+        hostname: "pridebites.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.muyinteresante.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
