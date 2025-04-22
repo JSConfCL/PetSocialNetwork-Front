13909b89384340a92b05a484d94c0191131cbdd4
kevinkeyssx <keyss1700@gmail.com>
2025-04-21 20:26:26 -0400

    LayoutController called

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { ThemeProvider } from "next-themes";
 
-import { Header, Menu, PawBackground } from "@/components";
+import { PawBackground } from "@/components";
+import { LayoutController } from "@/components/layout/LayoutController";
 
 export const metadata: Metadata = {
   title: "PetSocialNetwork",
@@ -15,23 +16,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="es" suppressHydrationWarning>
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          <PawBackground />
+					<PawBackground />
 
-          <Header />
-
-          <div className="container mx-auto p-4">
-            <div className="flex flex-col lg:flex-row gap-6">
-              <aside className="lg:sticky lg:top-20 lg:h-[calc(100vh-5rem)] lg:w-64">
-                <Menu />
-              </aside>
-
-              <main className="flex-1 lg:flex lg:justify-center">
-                <div className="w-full lg:max-w-[calc(100%-4rem)]">
-                  {children}
-                </div>
-              </main>
-            </div>
-          </div>
+					<LayoutController>
+						{children}
+					</LayoutController>
         </ThemeProvider>
       </body>
     </html>
